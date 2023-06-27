514d9662b2d624d2bec9123e6502aebe3ab54112
jimmybombimmy <jamespeters92@gmail.com>
2023-06-27 15:53:57 +0100

    create api route that links to express.json file.

--- db/models/data.model.js
+++ db/models/data.model.js
@@ -1,5 +1,14 @@
 const db = require('../connection.js')
+const fs = require("fs/promises");
 const {getTopics} = require('../queries/queries.js')
+
+exports.getApiModel = () => {
+  return fs.readFile(`endpoints.json`, "utf-8")
+    .then(endpoints => {
+      const parseId = JSON.parse(endpoints);
+      return parseId
+    })
+}
 
 exports.topicsModel = () => {
   return db.query(getTopics)
