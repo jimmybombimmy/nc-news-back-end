59f0c665ba82d0e62c59b1be75de968133ad8946
jimmybombimmy <jamespeters92@gmail.com>
2023-06-28 08:53:39 +0100

    create errors file

---
+++ db/errors.js
@@ -0,0 +1,21 @@
+exports.errorHandlers = (err, req, res, next) => {
+  console.log(err)
+  // Custom errors
+  if (err.status && err.message) {
+    res.status(err.status).send({
+      message: err.message
+    });
+  }
+  // psql 400 errors
+  else if (err.code === '22P02' || err.code === '42703') {
+    res.status(400).send({
+      message: 'Bad Request'
+    });
+  } 
+  // internal server error (for if error unidentified)
+  else {
+    res.status(500).send({
+      msg: 'Internal Server Error'
+    });
+  }
+}
