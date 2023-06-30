a2abc2c531f22fce486c02e36dc1d5631b66d413
jimmybombimmy <jamespeters92@gmail.com>
2023-06-30 10:47:43 +0100

    create route for posting a comment to an article

--- db/controllers/data.controller.js
+++ db/controllers/data.controller.js
@@ -4,7 +4,8 @@ const {
   getApiModel,
   getSoleArticleModel,
   getAllArticlesModel,
-  getArticlesCommentsModel
+  getArticlesCommentsModel,
+  postArticleCommentModel
 } = require('../models/data.model.js')
 
 
@@ -45,6 +46,16 @@ exports.getArticlesComments = (req, res, next) => {
   getArticlesCommentsModel(article_id)
     .then((comments) => {
       res.status(200).send({comments})
+    })
+    .catch(next)
+}
+
+exports.postArticleComment = (req, res, next) => {
+  const {article_id} = req.params;
+  const newComment = req.body
+  postArticleCommentModel(article_id, newComment)
+    .then((comment) => {
+      res.status(201).send(comment)
     })
     .catch(next)
 }
