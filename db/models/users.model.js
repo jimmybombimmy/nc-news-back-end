39208e2786b5cca8d86d3d0466dc972dec1ee94b
jimmybombimmy <jamespeters92@gmail.com>
2023-09-02 09:37:26 +0100

    remove console log in get users model

--- db/models/users.model.js
+++ db/models/users.model.js
@@ -3,7 +3,6 @@ const db = require('../connection.js')
 exports.getAllUsersModel = () => {
   return db.query(`SELECT * FROM users;`)
     .then(({rows}) => {
-      console.log(rows)
       return rows
     })
 }
