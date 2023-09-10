8afe24f227dbe922539dad6a65232c7830b9b919
jimmybombimmy <jamespeters92@gmail.com>
2023-09-10 13:34:36 +0100

    tidy tests by getting rid of unneeded requires

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,16 +1,10 @@
 const app = require('../app.js');
 const request = require('supertest');
-const express = require("express");
 
 const seed = require('../db/seeds/seed.js')
 const testData = require('../db/data/test-data')
 const connection = require('../db/connection.js')
 const endpoints = require('../endpoints.json');
-const {
-  Query
-} = require('pg');
-
-const articles = require('../db/data/test-data/articles.js')
 
 
 beforeEach(() => seed(testData))
